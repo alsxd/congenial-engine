ca79093573393fb3452b77c52de271f7dd62fd19
Albert Shefner <solaris945@gmail.com>
2023-12-20 11:49:13 +0300

    Добавлена проверка

--- main.c
+++ main.c
@@ -5,6 +5,10 @@ int main() {
 	int a, b, c;
 	printf("Введите 2 числа: ");
 	scanf("%d %d", &a, &b);
+	if(a == 0 || b == 0) {
+		printf("Не ноль введите");
+		return 1;
+	}
 	printf("a + b = %d\n", c);
 	c = a + b;
 	return 0;
